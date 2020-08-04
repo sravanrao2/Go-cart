628069c7396963b523495bf3b452f88373bcd797
surya <gutpes@gmail.com>
2020-08-04 11:02:28 -0400

    forgot password

--- app/src/main/java/com/example/gorcories/ForgotPassword.java
+++ app/src/main/java/com/example/gorcories/ForgotPassword.java
@@ -3,12 +3,23 @@ package com.example.gorcories;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ForgotPassword extends AppCompatActivity {
+public class ForgotPassword extends AppCompatActivity implements View.OnClickListener {
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"Password rsent link has been sent to email",Toast.LENGTH_SHORT).show();
     }
 }
