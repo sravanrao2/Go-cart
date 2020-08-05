a0e706068a2faa724cf32a7f85348ba4a719988f
surya <gutpes@gmail.com>
2020-08-04 23:13:05 -0400

    signupupdated

--- app/src/main/java/com/example/gorcories/Signupactivity.java
+++ app/src/main/java/com/example/gorcories/Signupactivity.java
@@ -2,13 +2,35 @@ package com.example.gorcories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Signupactivity extends AppCompatActivity {
+    Button signup;
+    TextView signinhere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signupactivity);
+        signup = findViewById(R.id.button2);
+        signinhere = findViewById(R.id.textView);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signupactivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        signinhere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signupactivity.this,SigninActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
