b2077ed510f12654aef53c61b6f7ca03db2acd2b
Venkat Akhileshwar <venkatrinku.akhil11@gmail.com>
2020-08-04 23:48:52 -0400

    profile added

---
+++ app/src/main/java/com/example/gorcories/Profile.java
@@ -0,0 +1,20 @@
+package com.example.gorcories;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class Profile extends Fragment {
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.profile, container, false);
+        return view;
+    }
+}
