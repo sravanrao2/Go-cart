3a617c81c5523aa283ca46e23d0e88928aecdaf8
Sai Rathan <venkatrinku.akhil11@gmail.com>
2020-08-04 23:44:27 -0400

    signin updated

--- app/src/main/java/com/example/gorcories/SigninActivity.java
+++ app/src/main/java/com/example/gorcories/SigninActivity.java
@@ -2,14 +2,36 @@ package com.example.gorcories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SigninActivity extends AppCompatActivity {
 
+    Button signin;
+    TextView signuphere;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        signin = findViewById(R.id.signin);
+        signuphere = findViewById(R.id.textView);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SigninActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        signuphere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SigninActivity.this,Signupactivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
 
