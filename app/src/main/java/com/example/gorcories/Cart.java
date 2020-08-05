20454814a195fa7f9b7de96bd3b73beddfffc19e
priyatham <saipriyathamvarma@gmail.com>
2020-08-04 22:17:14 -0400

    cart and flyer fragments

---
+++ app/src/main/java/com/example/gorcories/Cart.java
@@ -0,0 +1,21 @@
+package com.example.gorcories;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+
+public class Cart extends Fragment {
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.cart, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/gorcories/FlyerFragment.java
@@ -0,0 +1,20 @@
+package com.example.gorcories;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class FlyerFragment extends Fragment {
+
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.flyer, container, false);
+        return view;
+    }
+}
