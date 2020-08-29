ce5a3daa152c99e08089e0b031e3ea7e6cd9c25f
venkat akhileshwar thota <venkatrinku.akhil11@gmail.com>
2020-08-29 12:56:39 -0400

    created itemsview

--- app/src/main/java/com/example/gorcories/ItemsView.java
+++ app/src/main/java/com/example/gorcories/ItemsView.java
@@ -5,15 +5,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemsView extends Fragment {
     View view;
+    Button btn;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("weekdays");
+
+    private Itemadaptor adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.item, container, false);
+        view = inflater.inflate(R.layout.items_list, container, false);
+        Query query = notebookRef.orderBy("name", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<orderpojo> options = new FirestoreRecyclerOptions.Builder<orderpojo>()
+                .setQuery(query, orderpojo.class)
+                .build();
+        adapter = new Itemadaptor(options);
+        RecyclerView recyclerView = view.findViewById(R.id.recycleritems);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
         return view;
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
+
