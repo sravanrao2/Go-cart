cb48dfece24eb9d0bb78ff3b6d2acca626168b62
surya <gutpes@gmail.com>
2020-08-29 19:17:46 -0400

    recycleradaptor

---
+++ app/src/main/java/com/example/gorcories/Ingredients.java
@@ -0,0 +1,27 @@
+package com.example.gorcories;
+
+public class Ingredients {
+    private String name;
+    private int price;
+
+    public Ingredients(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
