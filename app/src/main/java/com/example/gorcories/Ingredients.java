b2b080fcf6c53548c7660b0092f4e2593c6eb297
surya <gutpes@gmail.com>
2020-08-30 02:21:55 -0400

    comments updated

--- app/src/main/java/com/example/gorcories/Ingredients.java
+++ app/src/main/java/com/example/gorcories/Ingredients.java
@@ -1,26 +1,54 @@
 package com.example.gorcories;
 
+/**
+ * Represents the Ingredients if receipe
+ */
 public class Ingredients {
+    /**
+     *Represents the Ingredients name
+     * Represents the ingredients price
+     */
     private String name;
     private int price;
 
+    /**
+     * Creates Ingredients with name and price
+     * @param name the ingredients name
+     * @param price the ingredients price
+     */
     public Ingredients(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
+    /**
+     * Get's Ingredients name
+     * @return a string represents Ingredients name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set's Ingredients name
+     * @param name a string contains Ingredients name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Get's Ingredients price
+     * @return a integer represents the Ingredients price
+     */
     public int getPrice() {
         return price;
     }
 
+    /**
+     * Set's the ingredients price
+     * @param price a integer represents the
+     */
     public void setPrice(int price) {
         this.price = price;
     }
--- app/src/main/java/com/example/gorcories/RecyclerAdaptor.java
+++ app/src/main/java/com/example/gorcories/RecyclerAdaptor.java
@@ -10,22 +10,37 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+/**
+ * This class acts as adapter class for cart items
+ * This class extends RecyclerView
+ */
 public class RecyclerAdaptor extends RecyclerView.Adapter<RecyclerAdaptor.ViewHolder> {
 
     List<Ingredients> ingredients;
     Cart mcontext;
     ItemsView vcontext;
+
+
     public RecyclerAdaptor(List<Ingredients> ingredients, Cart cart) {
         this.ingredients = ingredients;
         this.mcontext = cart;
     }
+
+
     public RecyclerAdaptor(List<Ingredients> ingredients, ItemsView items) {
         this.ingredients = ingredients;
         this.vcontext = items;
     }
 
+    /**
+     *
+     * @param parent The ViewGroup into which the new View will be added
+     * @param viewType The view type of the new View.
+     * @return returns ViwHolder instance
+     */
     @NonNull
     @Override
+
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View cartitems= layoutInflater.inflate(R.layout.cart, parent, false);
@@ -33,18 +48,29 @@ public class RecyclerAdaptor extends RecyclerView.Adapter<RecyclerAdaptor.ViewHo
         return viewHolder;
     }
 
+    /**
+     *
+     * @param holder The Viewholder that to be updated with cart items at given position
+     * @param position The position of the holder with respect to this adapter
+     */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.textView.setText(ingredients.get(position).getName());
     }
 
+    /**
+     * This method is used to get count of ingredients
+     * @return Ingredients size
+     */
     @Override
     public int getItemCount() {
         return ingredients.size();
     }
 
+    /**
+     * This class deals with cart item data and their instances
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder{
-
         public TextView textView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
